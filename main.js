0d0830eba77919b883e96124d13d8f65effc6374
shlok0070 <shlok0070@gmail.com>
2023-02-21 19:33:56 +0530

    Added Search filtering

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-
+var filter = document.getElementById('filter');
 // Form submit event
 form.addEventListener('submit', addItem);
 
 // Delete event
 itemList.addEventListener('click', removeItem);
+
+// Filter event
+
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e){
@@ -48,4 +52,26 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+// Filter Items
+
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase(); 
+
+    // Get li's
+    var items = itemList.getElementsByTagName('li');
+
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1)
+        {
+            item.style.display = 'block';
+        }
+        else
+        {
+            item.style.display = 'none';
+        }
+    });
 }
